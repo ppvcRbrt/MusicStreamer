939809700840249b1fc19137405d3a21f02d69e3
ppvcRbrt <73713049+ppvcRbrt@users.noreply.github.com>
2026-02-10 21:58:23 +0000

    Created initial streaming functionality

---
+++ MusicStreamerBackend/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "OrchestratorV2 API v1");
+    });
+
+}
+else
+{
+    app.UseHttpsRedirection();    
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
