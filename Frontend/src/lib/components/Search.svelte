245b339303da1ae86251470a6f542581f10a158e
ppvcRbrt <73713049+ppvcRbrt@users.noreply.github.com>
2026-03-04 17:37:40 +0000

    Updated the search component to take a class prop

--- Frontend/src/lib/components/Search.svelte
+++ Frontend/src/lib/components/Search.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import {SearchIcon} from "@lucide/svelte";
     import * as InputGroup from "$lib/components/ui/input-group/index.js";
-    let { searchQuery = $bindable() }: { searchQuery: string } = $props();
+    let { searchQuery = $bindable(), Class="" }:
+        { searchQuery: string, Class: string } = $props();
 </script>
-<InputGroup.Root class="backdrop-blur-3xl bg-secondary/50">
+<InputGroup.Root class="{Class}">
     <InputGroup.Input bind:value={searchQuery} placeholder="Search..." class="text-center"/>
     <InputGroup.Addon>
         <SearchIcon />
